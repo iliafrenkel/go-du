7ef44c13ea19c0378b78aa926d5d65bf482882d4
Ilia Frenkel <frenkel.ilia@gmail.com>
2021-05-13 21:31:16 +1000

    Test data creation.

--- app/main_test.go
+++ app/main_test.go
@@ -1,9 +1,39 @@
 package main
 
-func createTestFiles() {
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
+// Creates directories and file of particular size.
+func createTestData() error {
+	// create sub-directories
+	err := os.MkdirAll("./testdata/subdir/subsubdir", 0755)
+	if err != nil {
+		return err
+	}
+
+	// create files
+	tmpBuf := make([]byte, 3456)
+	ioutil.WriteFile("./testdata/under_4k.txt", tmpBuf, 0644)
+	tmpBuf = make([]byte, 4096)
+	ioutil.WriteFile("./testdata/exactly_4k.txt", tmpBuf, 0644)
+	tmpBuf = make([]byte, 5678)
+	ioutil.WriteFile("./testdata/over_4k.txt", tmpBuf, 0644)
+
+	tmpBuf = make([]byte, 4096*1024)
+	ioutil.WriteFile("./testdata/subdir/exactly_4m.txt", tmpBuf, 0644)
+
+	tmpBuf = make([]byte, 4096*1024+1)
+	ioutil.WriteFile("./testdata/subdir/subsubdir/over_4m.txt", tmpBuf, 0644)
+
+	return nil
 }
 
-func Test_Main() {
-
+func Test_Main(t *testing.T) {
+	err := createTestData()
+	if err != nil {
+		t.Fatalf("Failed to create test data: %v", err)
+	}
 }
